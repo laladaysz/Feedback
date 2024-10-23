c7435ff9d5bcb8d36df2b508cc5d3698b5874b80
laladaysz <leandralaura.dias@gmail.com>
2024-10-23 16:27:43 -0300

    :sparkles: feat: db config

--- src/main/java/br/ETS/Feedback/instrutor/Instrutor.java
+++ src/main/java/br/ETS/Feedback/instrutor/Instrutor.java
@@ -3,9 +3,21 @@ package br.ETS.Feedback.instrutor;
 
 import br.ETS.Feedback.informacoes.Informacoes;
 import br.ETS.Feedback.instrutor.DTO.DadosCadastroInstrutor;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity(name = "instrutor")
+@Table(name = "tbInstrutores")
 public class Instrutor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String nome;
     private String email;
     private String edv;
